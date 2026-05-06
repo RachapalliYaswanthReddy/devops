798fdd181562c61d8b82e0ca9cc8137c1a8a0756
yaswanth2020 <rachapalliyaswanthreddy2020@gmail.com>
2026-05-06 10:21:42 +0530

    divison operation

---
+++ Git_Practice/src/com/nt/main/Divison.java
@@ -0,0 +1,12 @@
+package com.nt.main;
+
+public class Divison {
+	static int a=30;
+	public static void main(String args[])
+	{
+		int b=20;
+		int c=50;
+		System.out.println("divison of a and b is :"+(a/b));
+	}
+
+}
